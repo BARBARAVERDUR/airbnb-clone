c0480a993a5de9c8ba9abfc21a448e11c74d651d
Barbara Verdur <barbiverdur@gmail.com>
2023-04-09 20:15:52 -0300

    Edit-Tab-Navigator

--- App.js
+++ App.js
@@ -1,55 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ProfileScreen } from './src/screens/profile/ProfileScreen';
-import { HomeScreen } from './src/screens/home/HomeScreen';
-import { LocationListStackScreen } from "./src/screens/location-list/LocationListStackScreen";
-import { Ionicons } from "@expo/vector-icons";
-import { COLORS, SPACING } from './src/utils/theme';
+import {  MainScreen } from "./src/screens/location-list/MainScreen";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LocationDetailScreen } from './src/screens/location-detail/LocationDetailScreen';
 
-const Tab = createBottomTabNavigator();
-
-const TAB_ICON ={
-  Home: "home",
-  Profile: "person",
-  Explorar : "search"
-}
-
-const screenOptions = ({route}) => {
-  const iconName = TAB_ICON[route.name] // TAB_ICON[Home]
-  return {
-    tabBarIcon: ({size, color}) => (
-      <Ionicons name ={iconName} size={size} color={color}/>
-    ),
-    tabBarActiveTintColor : COLORS.primary,
-    tabBarInactiveTintColor : COLORS.inactive,
-    headerShown : false,
-    tabBarStyle :StyleSheet.tabBar
-
-  }
-}
+const LocationListStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Tab.Navigator screenOptions={screenOptions}>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Explorar" component={LocationListStackScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
+        <LocationListStack.Navigator screenOptions={{ headerShown: false }}>
+          <LocationListStack.Screen
+            name="Principal"
+            component={MainScreen}
+          />
+{/*           //pantalla sin tab*/}      
+           <LocationListStack.Screen
+            name="Detail"
+            component={LocationDetailScreen}
+          />
+        </LocationListStack.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  tabBar: {
-    height: SPACING.xxxl,
-    paddingBottom: SPACING.xs,
-    paddingTop: SPACING.xs,
-  },
-});
 
---
+++ src/screens/location-list/MainScreen.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+import { LocationListScreen } from './LocationListScreen'
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { COLORS, SPACING } from '../../utils/theme';
+import { HomeScreen } from './../home/HomeScreen';
+import { ProfileScreen } from './../profile/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Home: "home",
+  Profile: "person",
+  ListExplorer: "search",
+};
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name]; // TAB_ICON[Home]
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: COLORS.primary,
+    tabBarInactiveTintColor: COLORS.inactive,
+    headerShown: false,
+    tabBarStyle: StyleSheet.tabBar,
+  };
+};
+
+export const MainScreen = () => {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="ListExplorer" component={LocationListScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBar: {
+    height: SPACING.xxxl,
+    paddingBottom: SPACING.xs,
+    paddingTop: SPACING.xs,
+  },
+});
+
